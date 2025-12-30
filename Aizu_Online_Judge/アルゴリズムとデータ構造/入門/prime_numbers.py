cafbf4a879863f963227018fd1d98fa11edd9006
testrs865 <testrs865@users.noreply.github.com>
2025-12-30 16:19:01 +0900

    break_flag挿入

--- Aizu_Online_Judge/アルゴリズムとデータ構造/入門/prime_numbers.py
+++ Aizu_Online_Judge/アルゴリズムとデータ構造/入門/prime_numbers.py
@@ -12,10 +12,13 @@ for i in range(n):
     if m % 2 == 0:
         continue
     j = 3
+    break_flag = 0
     while j * j <= m:
         if m % j == 0:
+            break_flag = 1
             break
         j += 2
-    prime_sum += 1
+    if break_flag == 0:
+        prime_sum += 1
     
 print(prime_sum)
